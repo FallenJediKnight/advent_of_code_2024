ff9fb4290fee734c8fece6218c2399eff57ba864
Albert Mitton <albert.mitton@xero.com>
2024-12-04 08:37:58 +1300

    Add timers around both puzzle solutions

--- 03/day-three/MullItOver/Program.cs
+++ 03/day-three/MullItOver/Program.cs
@@ -1,14 +1,37 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using MullItOver;
 
-int puzzle1Sum;
-int puzzle2Sum;
-using (StreamReader streamReader = new("03-input.txt", Encoding.UTF8))
-{
-    string corruptedMemory = streamReader.ReadToEnd();
-    puzzle1Sum = ParseCorruptedMemoryFile.Parse(corruptedMemory);
-    puzzle2Sum = ParseCorruptedMemoryFile.ParsePuzzle2(corruptedMemory);
-}
+DayThree.Puzzle1();
+DayThree.Puzzle2();
 
-Console.WriteLine(puzzle1Sum);
-Console.WriteLine(puzzle2Sum);
+internal static class DayThree
+{
+    public static void Puzzle1()
+    {
+        Stopwatch watch = Stopwatch.StartNew();
+        int puzzle1Sum;
+        using (StreamReader streamReader = new("03-input.txt", Encoding.UTF8))
+        {
+            string corruptedMemory = streamReader.ReadToEnd();
+            puzzle1Sum = ParseCorruptedMemoryFile.Parse(corruptedMemory);
+        }
+        Console.WriteLine(puzzle1Sum);
+        watch.Stop();
+        Console.WriteLine($"Puzzle 1 execution time: {watch.ElapsedMilliseconds} ms");
+    }
+    
+    public static void Puzzle2()
+    {
+        Stopwatch watch = Stopwatch.StartNew();
+        int puzzle2Sum;
+        using (StreamReader streamReader = new("03-input.txt", Encoding.UTF8))
+        {
+            string corruptedMemory = streamReader.ReadToEnd();
+            puzzle2Sum = ParseCorruptedMemoryFile.ParsePuzzle2(corruptedMemory);
+        }
+        Console.WriteLine(puzzle2Sum);
+        watch.Stop();
+        Console.WriteLine($"Puzzle 2 execution time: {watch.ElapsedMilliseconds} ms");
+    }
+}
